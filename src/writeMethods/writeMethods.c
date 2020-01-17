b2c32e417e3e6b7b4eccbdcadc0d57419cecab3c
Barak Sason Rofman <sason922@gmail.com>
2020-01-17 22:28:08 +0200

    Minor update to binary wirte method
    Signed-off-by: Barak Sason Rofman <sason922@gmail.com>

--- src/writeMethods/writeMethods.c
+++ src/writeMethods/writeMethods.c
@@ -10,49 +10,46 @@
 #include "writeMethods.h"
 #include "../core/api/logger.h"
 
-static char logLevelsIds[] = { ' ', /* NONE */
-                               'M', /* EMERGENCY */
-                               'A', /* ALERT */
-                               'C', /* CRITICAL */
-                               'E', /* ERROR */
-                               'W', /* WARNING */
-                               'N', /* NOTICE */
-                               'I', /* INFO */
-                               'D', /* DEBUG */
-                               'T', /* TRACE */};
+static const char logLevelsIds[] = { ' ', /* NONE */
+                                     'M', /* EMERGENCY */
+                                     'A', /* ALERT */
+                                     'C', /* CRITICAL */
+                                     'E', /* ERROR */
+                                     'W', /* WARNING */
+                                     'N', /* NOTICE */
+                                     'I', /* INFO */
+                                     'D', /* DEBUG */
+                                     'T', /* TRACE */};
 
-static char* logMethods[] = { "pb", "sb", "dw" };
+static const char* logMethods[] = { "pb", "sb", "dw" };
 
 /* API method - Description located at .h file */
 void asciiWrite(const MessageData* md, FILE* logFile) {
 	int maxMsgLen = getMaxMsgLen();
-	{
-		int msgLen;
-		char buf[maxMsgLen];
+	int msgLen;
+	char buf[maxMsgLen];
 
-		msgLen =
-		        snprintf(
-		                buf,
-		                maxMsgLen,
-		                "[mid: %x:%.5x] [ll: %c] [lm: %s] [lwp: %.5ld] [loc: %s:%s:%d] [msg: %s]\n",
-		                (unsigned int) md->tv.tv_sec,
-		                (unsigned int) md->tv.tv_usec,
-		                logLevelsIds[md->logLevel], logMethods[md->logMethod],
-		                md->tid, md->file, md->func, md->line, md->argsBuf);
+	msgLen = snprintf(buf, maxMsgLen,
+	                  "[mid: %x:%.5x] [ll: %c] [lm: %s] [lwp: %.5ld] [loc: %s:%s:%d] [msg: %s]\n",
+	                  (unsigned int) md->tv.tv_sec, (unsigned int) md->tv.tv_usec,
+	                  logLevelsIds[md->logLevel], logMethods[md->logMethod], md->tid, md->file,
+	                  md->func, md->line, md->argsBuf);
 
-		fwrite(buf, 1, msgLen, logFile);
-	}
+	fwrite(buf, 1, msgLen, logFile);
 }
 
 /* API method - Description located at .h file */
 void binaryWrite(const MessageData* md, FILE* logFile) {
-	int fileNameLen = strlen(md->file);
-	int methodNameLen = strlen(md->func);
-	int argsBufLen = strlen(md->argsBuf);
+	int fileNameLen;
+	int methodNameLen;
+	int argsBufLen;
 
-	fwrite(&md->tv.tv_sec, sizeof(md->tv.tv_sec), 1, logFile);
-	fwrite(&md->tv.tv_usec, sizeof(md->tv.tv_usec), 1, logFile);
-	fwrite(&logLevelsIds[md->logLevel], 1, 1, logFile);
+	fileNameLen = strlen(md->file);
+	methodNameLen = strlen(md->func);
+	argsBufLen = strlen(md->argsBuf);
+
+	fwrite(&md->tv, sizeof(md->tv), 1, logFile);
+	fwrite(&logLevelsIds[md->logLevel], sizeof(logLevelsIds[0]), 1, logFile);
 	fwrite(logMethods[md->logMethod], 2, 1, logFile);
 	fwrite(&md->tid, sizeof(md->tid), 1, logFile);
 	fwrite(&fileNameLen, sizeof(fileNameLen), 1, logFile);
