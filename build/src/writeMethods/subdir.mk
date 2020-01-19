ddb3ae9797f08de05ba400e39a8e74c225d1d943
Barak Sason Rofman <sason922@gmail.com>
2020-01-19 13:25:30 +0200

    Add to last commit
    Signed-off-by: Barak Sason Rofman <sason922@gmail.com>

---
+++ build/src/writeMethods/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/writeMethods/writeMethods.c 
+
+OBJS += \
+./src/writeMethods/writeMethods.o 
+
+C_DEPS += \
+./src/writeMethods/writeMethods.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/writeMethods/%.o: ../src/writeMethods/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -D_GNU_SOURCE -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
